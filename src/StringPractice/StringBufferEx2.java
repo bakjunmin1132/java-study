eac1fc520bf376153ec5f90b60da7f3ff0b2b9d3
bakjunmin1132 <cyan104@naver.com>
2026-08-30 13:16:58 +0900

    StringBuffer, StringBuilder 완료

--- src/StringPractice/StringBufferEx2.java
+++ src/StringPractice/StringBufferEx2.java
@@ -1,4 +1,20 @@
 package StringPractice;
 
 public class StringBufferEx2 {
+    static void main() {
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("hello");
+//        sb.append(" ");
+//        sb.append("jump to java");
+//        String result = sb.toString();
+//        System.out.println(result);
+
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("jump to java");
+//        sb.insert(0, "hello ");
+//        sb.delete(4, 5);
+//        sb.delete(9, 17);
+//        System.out.println(sb.toString());
+//        System.out.println(sb.substring(5, 9));
+    }
 }
