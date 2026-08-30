a19bfc806418efeeaba397da88756baa13b83d15
bakjunmin1132 <cyan104@naver.com>
2026-08-30 12:51:14 +0900

    finsihed StringEx1

---
+++ src/StringPractice/StringEx1.java
@@ -0,0 +1,32 @@
+package StringPractice;
+
+public class StringEx1 {
+    static void main() {
+//        String a = "Hello";
+//        String b = new String("Hello");
+//        System.out.println(a.equals(b));
+//        System.out.println(a == b);
+
+
+//        String a = "Hello Java";
+//
+//        System.out.println(a.indexOf("Java")); // 6
+//        System.out.println(a.contains("Java")); // true
+//        System.out.println(a.charAt(6)); // J
+//        System.out.println(a.replaceAll("Java", "World")); // Hello, World
+//        System.out.println(a.substring(0, 4)); // Hell
+//        System.out.println(a.toUpperCase()); // HELLO JAVA
+//        System.out.println(a.toLowerCase()); // hello java
+
+
+        String a = "a:b:c:d";
+
+//        String[] result = a.split(":");
+//        for (String i : result) {
+//            System.out.printf("%s ", i);
+//        }
+//        System.out.println();
+        
+
+    }
+}
