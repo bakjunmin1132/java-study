6aaf9a0fd29ef952f673fb1fb65b7d1a640ad365
bakjunmin1132 <cyan104@naver.com>
2026-09-03 09:41:43 +0900

    update InterFace

---
+++ src/Learn_Interface/InterFace.java
@@ -0,0 +1,47 @@
+package Learn_Interface;
+
+interface Predator {
+    String getFood(); // 메서드에 입출력이 없는 이유는 인터페이스는 규칙이기 때문
+
+    default void printFood() { // 디폴트 메서드
+        System.out.println("my food is "+getFood());
+    }
+}
+
+class Animal {
+    String name;
+
+    void setName(String name) {
+        this.name = name;
+    }
+}
+
+class Tiger extends Animal implements Predator { // 나는 동물 자료형을 가지면서 포식자로도 취급할 수 있다
+    public String getFood() { // 인터페이스의 메서드는 항상 public으로 구현해야 한다
+        return "apple";
+    }
+}
+
+class Lion extends Animal implements Predator {
+    public String getFood() {
+        return "banana";
+    }
+}
+
+class Zookeeper {
+    void feed(Predator predator) {
+        System.out.println("feed "+predator.getFood());
+    }
+}
+
+public class InterFace {
+    static void main() {
+        Zookeeper zookeeper = new Zookeeper();
+        Tiger tiger = new Tiger();
+        Lion lion = new Lion();
+        zookeeper.feed(tiger);
+        zookeeper.feed(lion);
+        tiger.printFood();
+        lion.printFood();
+    }
+}
